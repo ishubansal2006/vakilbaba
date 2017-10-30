061bfe0900d6d9ece1030cabf679500c9e28d4ac
ishubansal2006 <33174583+ishubansal2006@users.noreply.github.com>
2017-10-30 12:56:55 +0530

    Customer sign up chnages

--- customer-signup.php
+++ customer-signup.php
@@ -25,7 +25,7 @@
 </head>
 <body>  
   <!--header-->
-<?php include 'includes/header.php';?>
+<?php include('includes/header.php');?>
 <!--/header--> 
 
     <div class="container" style="margin-top: 100px">
@@ -73,6 +73,7 @@
 </div>  
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-form.js"></script>
 <script type="text/javascript" src="assets/js/customersignup.js"></script>
 <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
